cceb9c7f32f8496a347b3fadbc655b4af157a831
ur <dorosi150@gmail.com>
2023-05-27 00:47:42 +0900

    Feat: Create camera

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { Camera, useCameraDevices } from "react-native-vision-camera";
 
 export default function App() {
-  return (
+  const devices = useCameraDevices();
+  const device = devices.back;
+
+  const [cameraPermission, setCameraPermission] = useState(null);
+
+  async function requestCameraPermission() {
+    const newCameraPermission = await Camera.requestCameraPermission();
+
+    setCameraPermission(newCameraPermission);
+  }
+
+  return cameraPermission === "authorized" ? (
+    <View>
+      <View style={styles.red}>
+        <Text>지도?</Text>
+      </View>
+      <Camera style={styles.fillHalfScreen} device={device} isActive={true} />
+    </View>
+  ) : (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Camera permission: {cameraPermission}</Text>
+      <Button
+        onPress={requestCameraPermission}
+        title="Request camera permission"
+      />
     </View>
   );
 }
@@ -13,8 +36,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  fillHalfScreen: {
+    height: "50%",
+    width: "100%",
+  },
+  red: {
+    backgroundColor: "red",
+    height: "50%",
+    width: "100%",
   },
 });
