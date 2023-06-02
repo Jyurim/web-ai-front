32de291411d6bad77a56db4062da1643ddaade5f
ur <dorosi150@gmail.com>
2023-06-02 15:28:25 +0900

    Design: Change view

--- App.js
+++ App.js
@@ -1,34 +1,33 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { Camera, useCameraDevices } from "react-native-vision-camera";
+// import Map from "./components/Map";
 
 export default function App() {
   const devices = useCameraDevices();
   const device = devices.back;
-
   const [cameraPermission, setCameraPermission] = useState(null);
 
-  async function requestCameraPermission() {
-    const newCameraPermission = await Camera.requestCameraPermission();
-
-    setCameraPermission(newCameraPermission);
-  }
-
+  useEffect(() => {
+    const checkPermission = async () => {
+      // 카메라 권한 확인
+      const newCameraPermission = await Camera.requestCameraPermission();
+      setCameraPermission(newCameraPermission);
+    };
+    checkPermission();
+  }, []);
   return cameraPermission === "authorized" ? (
     <View>
       <View style={styles.red}>
-        <Text>지도?</Text>
+        <Text>지도</Text>
       </View>
       <Camera style={styles.fillHalfScreen} device={device} isActive={true} />
     </View>
   ) : (
     <View style={styles.container}>
-      <Text>Camera permission: {cameraPermission}</Text>
-      <Button
-        onPress={requestCameraPermission}
-        title="Request camera permission"
-      />
+      <View style={{ backgroundColor: "red", height: "100%", width: "100%" }}>
+        <Text>지도</Text>
+      </View>
     </View>
   );
 }
